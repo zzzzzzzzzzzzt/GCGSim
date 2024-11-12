5ef4502c1637ed371f002b52547ba71229308891
zzzzzzzzzzzzt <1724127350@qq.com>
2024-11-12 11:20:38 +0800

    exp.sh

---
+++ exp.sh
@@ -0,0 +1,9 @@
+python exp_embedding.py \
+--dataset IMDBMulti \
+--pretrain_path model_saved/IMDBMulti/2024-11-01/cpgsim_IMDBMulti_FFNGIN_2 \
+--gpu_id 0 \
+
+
+# --dataset AIDS700nef --pretrain_path model_saved/AIDS700nef/2024-07-18/CPRGsim_AIDS700nef_4
+# --dataset LINUX --pretrain_path model_saved/LINUX/2024-11-07/cpgsim_LINUX_woNGSA_1
+# --dataset IMDBMulti --pretrain_path model_saved/IMDBMulti/2024-11-01/cpgsim_IMDBMulti_FFNGIN_2
