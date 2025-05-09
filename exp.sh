0864f92217702ae69996388f9fa27dee1d80099f
zzzzzzzzzzzzt <zzzzzzzzzzzzt>
2025-05-10 01:19:25 +0800

    supplement LINUX IMDBMulti

--- exp.sh
+++ exp.sh
@@ -1,6 +1,6 @@
 python exp_embedding.py \
---dataset IMDBMulti \
---pretrain_path model_saved/IMDBMulti/2024-11-01/cpgsim_IMDBMulti_FFNGIN_2 \
+--dataset AIDS700nef \
+--pretrain_path model_saved/AIDS700nef/2025-04-28/CPRGsim_AIDS700nef_ppre0.0001__0 \
 --gpu_id 0 \
 --model_name CPRGsim
 
